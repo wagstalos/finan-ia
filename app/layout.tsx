fb39e29a047dd073bd6aa948a6c19fab2522b4c3
Wagner Paulo <wagstalos@hotmail.com>
2024-11-06 14:55:14 -0300

    feat: update font to mulish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {Mulish} from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const mulish = Mulish({
+  subsets: ["latin-ext"],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiasedc dark`}
+        className={`${mulish.className} antialiasedc dark`}
       >
         <ClerkProvider
           appearance={{
