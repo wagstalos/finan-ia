65cc665e0837f3692ab8a2f0c8f7abaeaa25f92f
Wagner Paulo <wagstalos@hotmail.com>
2024-11-06 12:16:18 -0300

    feat:add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
-import { LogInIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
@@ -20,15 +27,21 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
-
       {/* DIREITA */}
       <div className="relative h-full w-full">
-        <Image src="/login.png" alt="login" className="object-cover" fill />
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import { Button } from "./_components/ui/button"
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-  <Button>Hello World</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
   );
 };
 
